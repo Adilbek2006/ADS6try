c7bac256288a6cbd0a5fc0ddd67d7451da9a4501
Matebook <adilbek200620@gmail.com>
2025-04-11 16:41:45 +0500

    additional methods for mylist

--- src/Lists/MyList.java
+++ src/Lists/MyList.java
@@ -1,8 +1,15 @@
 package Lists;
+import java.util.Iterator;
 
 public interface MyList<T> {
     void add(T element);
+    void add(int index, T element);
     void remove(int index);
+    boolean remove(T element);
     T get(int index);
     int size();
+    boolean isEmpty();
+    void clear();
+    Iterator<T> iterator();
 }
+
